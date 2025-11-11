fbc62dd6ea829d0a22f3dfdc59caa0a912e80d82
Notagai <89167930+Notagai@users.noreply.github.com>
2025-11-11 16:59:57 -0500

    made game

--- script.js
+++ script.js
@@ -28,19 +28,78 @@ document.querySelector("h1").onmouseover = event => {
 }
 
 let boba = 0;
-let quality = 0.001;
+let quality = 0.01;
+let money = 0;
 let btnAllowed = true;
+let markAllowed = true;
+let marketcost = 0.10;
 
 function brew() {
     if(btnAllowed == true) {
         btnAllowed = false;
-        document.getElementById('button').style.cursor = 'not-allowed';
+        document.getElementById('brew').style.cursor = 'not-allowed';
+        document.getElementById('brew').innerText = 'brewing...';
+        document.getElementById('brew').style.backgroundColor = '#dbd0baff';
         setTimeout(() => {
             boba++;
-            document.getElementById("thing").innerText = 'you currently have ' + boba + ' boba.';
-            document.getElementById('button').style.cursor = 'pointer';
-        }, 5000);
+            document.getElementById("thing").innerText = 'you currently have ' + boba + ' boba and $' + Math.round(money * 100) / 100 + '.';
+            document.getElementById('brew').style.cursor = 'pointer';
+            document.getElementById('brew').style.backgroundColor = '#ffd079';
+            btnAllowed = true;
+            document.getElementById('sell').style.display = 'block';
+            document.getElementById('sellDesc').style.display = 'block';
+            document.getElementById('brew').innerText = 'brew boba';
+        }, 500); //change back to 500 l8r
     } else {
         alert("your boba is brewing! please wait before brewing more.");
     }     
 }
+
+function marketingUnlock() {
+  document.getElementById('mark').style.display = 'block';
+  document.getElementById('markDesc').style.display = 'block';
+}
+
+function sell() {
+    if(boba > 0) {
+        boba--;
+        money += quality;
+        money = Math.round(money * 100) / 100;
+        document.getElementById("thing").innerText = 'you currently have ' + boba + ' boba and $' + Math.round(money * 100) / 100 + '.';
+        if(money == 0.10) {
+          marketingUnlock()
+        }
+    } else {
+        alert("you don't have any boba to sell! brew some first.");
+    }
+}
+
+function market() {
+  if(markAllowed == true && money >= marketcost) {
+        markAllowed = false;
+        document.getElementById('mark').style.cursor = 'not-allowed';
+        document.getElementById('mark').style.backgroundColor = '#dbd0baff';
+        document.getElementById('mark').innerText = 'running ads...';
+        money -= marketcost;
+        setTimeout(() => {
+            document.getElementById("thing").innerText = 'you currently have ' + boba + ' boba and $' + Math.round(money * 100) / 100 + '.';
+            document.getElementById('mark').style.cursor = 'pointer';
+            document.getElementById('mark').style.backgroundColor = '#ffd079';
+            document.getElementById('mark').innerText = 'advertise';
+            quality += 0.01;
+            markAllowed = true;
+            marketcost = Math.round((marketcost * 1.15) * 100) / 100;
+            document.getElementById('mark').innerText = 'advertise for $' + marketcost;
+            document.getElementById('qua').innerHTML = 'each boba you sell earns you $' + Math.round(quality * 100) / 100 + '.';
+        }, 5000); //change back to 5000 l8r
+    } else {
+        alert("your marketing is in progress! please wait before advertising more.");
+    }
+}
+
+const checkforwin = setInterval(() => {
+    if(money >= 25) {
+        clearInterval(checkforwin);
+        document.getElementById("winmsg").style.display = 'block';
+    }
+}, 1);
